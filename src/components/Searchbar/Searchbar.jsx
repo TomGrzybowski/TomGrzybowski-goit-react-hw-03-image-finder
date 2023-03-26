8e403dc0591866f2646d4ac0c217da3d7abd1e96
TomekGrzybek <t.grzybowski94@gmail.com>
2023-03-27 00:54:58 +0200

    adding search icon

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '../../../node_modules/@fortawesome/react-fontawesome/index.js';
+import { faMagnifyingGlass } from '../../../node_modules/@fortawesome/free-solid-svg-icons/index.js';
 
 const Searchbar = ({ handleSubmit }) => {
   return (
     <header className="searchbar">
       <form className="search-form" onSubmit={handleSubmit}>
         <button type="submit" className="search-form-button">
-          <span className="search-form-button-label"></span>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            style={{ color: '#000000' }}
+          />
         </button>
 
         <input
